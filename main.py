1f20e75f3979b6f18c5f62c62f854edb4f5ad76b
Pavel <pasha10428@gmail.com>
2024-04-05 21:32:53 +0300

    login

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, redirect, request, abort
 from data import db_session
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from data.users import User
+from forms.login import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 def main():
@@ -13,6 +18,28 @@ def main():
 @app.route("/")
 def index():
     return render_template('base.html', title='AAAA')
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 
 
 if __name__ == '__main__':
