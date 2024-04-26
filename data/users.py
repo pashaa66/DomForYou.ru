65428fa2e16cee5a25c03b35dd5faae504053f69
Pavel <pasha10428@gmail.com>
2024-04-26 12:27:36 +0300

    advertisement V 1(finished work)

--- data/users.py
+++ data/users.py
@@ -22,7 +22,7 @@ class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     deals = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)#R
     role = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    # announcement = orm.relationship("Announcement", back_populates='user')
+    announcement = orm.relationship("Announcement", back_populates='user')
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=dt.datetime.date(dt.datetime.now()))
 
--- forms/announcement.py
+++ forms/announcement.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, IntegerField, SubmitField, SelectField, BooleanField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, NumberRange
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 
@@ -8,17 +8,19 @@ class CreateAnnouncementForm(FlaskForm):
 
     title = StringField('Название объекта', validators=[DataRequired()])
     description = StringField('Описание', validators=[DataRequired(), Length(max=300)])
-    location = StringField('Адрес', validators=[DataRequired()])
-    price = IntegerField('Цена', validators=[DataRequired()])
-    advertisement_type = SelectField('Тип объявления', choices=[('flat', 'Квартира'), ('house', 'Дом')],
+    city = StringField("Город", validators=[DataRequired()])
+    street = StringField("Улица", validators=[DataRequired()])
+    number_of_house = StringField("Номер дома", validators=[DataRequired(), Length(min=1, max=999)])
+    price = IntegerField('Цена', validators=[DataRequired(), NumberRange(min=1)])
+    announcement_type = SelectField('Тип объявления', choices=[('flat', 'Квартира'), ('house', 'Дом')],
                                      validators=[DataRequired()])
     square = FloatField('Площадь', validators=[DataRequired()])
-    kitchen_square = FloatField('Площадь кухни', validators=[DataRequired()], optional=True)
+    kitchen_square = FloatField('Площадь кухни', validators=[DataRequired()], default=1)
     number_of_rooms = IntegerField('Количество комнат', validators=[DataRequired()])
-    floor = IntegerField('Этаж', validators=[DataRequired()], optional=True)
-    number_of_floors = IntegerField('Этажи', validators=[DataRequired()], optional=True)
-    year_of_construction = IntegerField('Год постройки', validators=[DataRequired()], optional=True)
-    file = FileField('Фотография', validators=[FileRequired(), FileAllowed(['jpg', 'png'],
+    floor = IntegerField('Этаж', validators=[DataRequired()], default=1)
+    number_of_floors = IntegerField('Этажи', validators=[DataRequired()], default=1)
+    year_of_construction = IntegerField('Год постройки', validators=[DataRequired()], default=2000)
+    photo = FileField('Фотография', validators=[FileRequired(), FileAllowed(['jpg', 'png'],
                                                                            'Только изображения')])
     is_sell = BooleanField('Продаётся')
     submit = SubmitField('Создать')
